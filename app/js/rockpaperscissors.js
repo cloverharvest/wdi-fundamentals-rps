e65b21e01e8ffc5b2308cab67a4797213818c178
cloverharvest <anna@schwabdev.com>
2015-12-16 15:49:25 -0800

    wrote code to retrieve player and computer move

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -23,16 +23,32 @@ function randomPlay() {
 
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
+    var move;
     // If a `move` has a value, your expression should evaluate to that value.
+    if (move != null) {
+        move = move;
+    }
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    else {
+        move = getInput ();
+    }
+    return move;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
+    var move;
     // If a `move` has a value, your expression should evaluate to that value.
+    if (move != null) {
+      move = move}
+    }
+
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    else {
+      move = randomPlay();
+    }
+    console.log ("Computer choice:" + move);
+    return move ;
 }
 
 function getWinner(playerMove,computerMove) {
@@ -52,4 +68,3 @@ function playToFive() {
     /* YOUR CODE HERE */
     return [playerWins, computerWins];
 }
-
